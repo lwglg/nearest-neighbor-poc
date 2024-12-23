49b6a355cb4988cf46cfe0d9395a5969e493ab5d
Guilherme Lima Gonçalves <lwglguilherme@gmail.com>
2024-12-23 16:01:55 -0300

    refactor: make the code structure more modular

--- src/main.c
+++ src/main.c
@@ -1,109 +1,137 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "nearest-neighbor.h"
 
-void exportaVizinhosMaisProximos(char *linha, int tamanho, int *vetorIndicesDistancia, float *vetorVizinhoMaisProximo) {
-	FILE * arqout = fopen("resources/vizinho-mais-proximo.csv", "wb");
-	char cabecalho[] = "LINHA,TAMANHO\n"; 
+void exportaVizinhosMaisProximos(char *linha, int tamanho,
+                                 int *vetorIndicesDistancia,
+                                 float *vetorVizinhoMaisProximo) {
+  FILE *arqout = fopen("resources/vizinho-mais-proximo.csv", "wb");
+  char cabecalho[] = "LINHA,TAMANHO\n";
 
-	if (arqout == NULL) {
-		printf("\nErro ao abrir o arquivo para escrita!\n");
-    	exit(1);
-	}
+  if (arqout == NULL) {
+    printf("\nErro ao abrir o arquivo para escrita!\n");
+    exit(1);
+  }
 
-	fwrite(cabecalho, strlen(cabecalho), 1, arqout);
-	
-	for (int p = 0; p < tamanho; p++) {
-  		sprintf(linha,"%d,%.15f\n", vetorIndicesDistancia[p], vetorVizinhoMaisProximo[p]);
-  		fwrite(linha, strlen(linha), 1, arqout);
-	}
+  fwrite(cabecalho, strlen(cabecalho), 1, arqout);
 
- 	fclose(arqout);
+  for (int p = 0; p < tamanho; p++) {
+    sprintf(linha, "%d,%.15f\n", vetorIndicesDistancia[p],
+            vetorVizinhoMaisProximo[p]);
+    fwrite(linha, strlen(linha), 1, arqout);
+  }
+
+  fclose(arqout);
 }
 
-void realizaLeituraSequencias(char *linha, double *numbers, int tamanhoVetor, int debug) {
-	char *result;
-	FILE * arqin = fopen("resources/dados-entrada.txt", "rt");
+double *realizaLeituraSequencias(char *linha, int tamanhoVetor, int debug) {
+  char *result;
+  FILE *arqin = fopen("resources/dados-entrada.txt", "rt");
 
-	if (arqin == NULL)    {
-		printf("Erro na abertura do arquivo\n");
-		return;
-	}
-  
-	tamanhoVetor = 0;
+  double *numbers = (double *)malloc(100 * sizeof(double));
 
-	while (!feof(arqin)) {
-		// Lê uma linha (com o '\n')
-		result = fgets(linha, 100, arqin);  // Lê até 99 caracteres ou até o '\n'
+  if (arqin == NULL) {
+    printf("Erro na abertura do arquivo\n");
+    return numbers;
+  }
 
-		if (result) {
-			if (debug)
-		  		printf("%s\n", linha);
-		  	
-		  	numbers[tamanhoVetor] = atof(linha);
-		}
+  tamanhoVetor = 0;
 
-		tamanhoVetor++;
+  while (!feof(arqin)) {
+    // Lê uma linha (com o '\n')
+    result = fgets(linha, 100, arqin); // Lê até 99 caracteres ou até o '\n'
 
-		if ((tamanhoVetor % 100) == 0)
-	  		numbers = (double*)realloc(numbers, ((tamanhoVetor/100)*100 + 100) * sizeof(double));
+    if (result) {
+      if (debug)
+        printf("%s\n", linha);
 
-		numbers[tamanhoVetor] = atof(linha);
-	}
+      numbers[tamanhoVetor] = atof(linha);
+    }
+
+    tamanhoVetor++;
+
+    if ((tamanhoVetor % 100) == 0)
+      numbers = (double *)realloc(numbers, ((tamanhoVetor / 100) * 100 + 100) *
+                                               sizeof(double));
+
+    numbers[tamanhoVetor] = atof(linha);
+  }
+
+  return numbers;
+}
+
+double **calculaValoresDistancia(int tamanho, int sequencia, double *numbers,
+                                 int debug) {
+  int k;
+
+  // Matriz que armazena o cálculo das raízes com base na comparação com
+  // vetorVizinhoMaisProximo
+  double **vetorResultadoValoresDistancia = malloc(tamanho * sizeof(double *));
+
+#pragma omp parallel private(k)
+  {
+#pragma omp for
+    for (k = 0.; k < tamanho; k++) {
+      vetorResultadoValoresDistancia[k] = malloc(tamanho * sizeof(double));
+    }
+  }
+
+  int t, u;
+#pragma omp parallel private(t, u) shared(numbers, sequencia)
+  {
+#pragma omp for
+    for (t = 0; t < tamanho; t++) {
+      for (u = 0; u < tamanho; u++) {
+        vetorResultadoValoresDistancia[t][u] =
+            calculaRaiz(numbers, t, u, sequencia, debug);
+
+        if (debug)
+          printf("%.1f\n", vetorResultadoValoresDistancia[t][u]);
+      }
+    }
+  }
+
+  return vetorResultadoValoresDistancia;
 }
 
 void main(int argc, char *argv[]) {
-	FILE * arqout;
-	char *result;
-	char linha[100];
-	double *numbers;
-	int i, tamanhoVetor, sequencia;
-	int debug = 1;
+  FILE *arqout;
+  char *result;
+  char linha[100];
+  int tamanhoVetor, sequencia;
+  int debug = 1;
 
-	printf("Informe a sequencia: "); 
-	scanf("%d", &sequencia);
-	printf("----------------------------------------------------------\n ");
+  printf("Informe a sequencia: ");
+  scanf("%d", &sequencia);
+  printf("----------------------------------------------------------\n ");
 
-	numbers = (double*)malloc(100 * sizeof(double));  
-	tamanhoVetor = 0;
+  double *numbers;
+  numbers = realizaLeituraSequencias(linha, tamanhoVetor, debug);
 
-	realizaLeituraSequencias(linha, numbers, tamanhoVetor, debug);
+  tamanhoVetor = 0;
+  int tamanho = calculaTamanhoVetor(
+      sequencia, tamanhoVetor); // Calcula o tamanho do vetor com base na
+                                // sequência informada
+  int vetorResultadosParaSoma[sequencia]; // Vetor que receberá as somas das
+                                          // raízes
+  float *vetorVizinhoMaisProximo = malloc(
+      tamanho *
+      sizeof(
+          float *)); // Vetor que armazenará as raízes do vizinho mais próximo
 
-	int tamanho = calculaTamanhoVetor(sequencia, tamanhoVetor);						// Calcula o tamanho do vetor com base na sequência informada
-	int vetorResultadosParaSoma[sequencia];											// Vetor que receberá as somas das raízes
-	int *vetorIndicesDistancia = malloc(tamanho * sizeof (int*));					// Vetor que armazenas os índices do vizinho mais próximo
-	float *vetorVizinhoMaisProximo = malloc(tamanho * sizeof (float*));				// Vetor que armazenará as raízes do vizinho mais próximo
-	double **vetorResultadoValoresDistancia =  malloc(tamanho * sizeof (double*));	// Matriz que armazena o cálculo das raízes com base na comparação com vetorVizinhoMaisProximo
+  // Calcula raízes e popula vetorResultadoValoresDistancia
+  double **vetorResultadoValoresDistancia;
+  vetorResultadoValoresDistancia =
+      calculaValoresDistancia(tamanho, sequencia, numbers, debug);
 
-	int k;
+  // Calcula vizinho mais próximo, extraindo o seu índice
+  int *vetorIndicesDistancia;
+  vetorIndicesDistancia = extraiVizinhoMaisProximo(
+      tamanho, vetorResultadoValoresDistancia, vetorVizinhoMaisProximo, debug);
 
-	#pragma omp parallel private(k)
-	{
-		#pragma omp for
-		for (k = 0.; k < tamanho; k++) {
-	 		 vetorResultadoValoresDistancia[k] = malloc(tamanho * sizeof (double));
-		}
-	}
-
-  	int t, u;
-  	#pragma omp parallel private(t,u) shared(numbers, sequencia)
-  	{
-	    #pragma omp for
-	    // Calcular raízes e popular vetorResultdoValoresDistancia 
-	    for (t = 0; t  < tamanho; t++){
-	     	for (u = 0; u  < tamanho; u++){
-	        	vetorResultadoValoresDistancia[t][u] = calculaRaiz(numbers, t, u, sequencia, debug);
-	        	
-	        	if (debug)
-	        		printf("%.1f\n", vetorResultadoValoresDistancia[t][u]);
-	      	}
-	   	}
-  	}
-
-	// Calcula vizinho mais próximo, extraindo o seu índice	
-	extraiVizinhoMaisProximo(tamanho, vetorResultadoValoresDistancia, vetorVizinhoMaisProximo, vetorIndicesDistancia, debug);
-
-	// Exportação dos NN em um arquivo CSV
-	exportaVizinhosMaisProximos(linha, tamanho, vetorIndicesDistancia, vetorVizinhoMaisProximo);
+  // Exportação dos NN em um arquivo CSV
+  exportaVizinhosMaisProximos(linha, tamanho, vetorIndicesDistancia,
+                              vetorVizinhoMaisProximo);
 }
--- src/nearest-neighbor.c
+++ src/nearest-neighbor.c
@@ -1,75 +1,81 @@
-#include <stdio.h>
-#include <math.h>
 #include "nearest-neighbor.h"
 
-int calculaTamanhoVetor(int sequencia, int tamanhoVetorValores){
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int calculaTamanhoVetor(int sequencia, int tamanhoVetorValores) {
   return (tamanhoVetorValores - sequencia) + 1;
 }
 
-double calculaRaiz(double *vetor, int i, int j, int sequencia, int debug){
-	double valorSoma = 0;
-	double raiz = 0;
-	int l;
+double calculaRaiz(double *vetor, int i, int j, int sequencia, int debug) {
+  double valorSoma = 0;
+  double raiz = 0;
+  int l;
 
+#pragma omp parallel private(l) shared(i, j, sequencia, vetor)
+  {
+#pragma omp for
+    for (int l = 0; l < sequencia; l++) {
+      if (debug == 1)
+        printf("Valor I %.2f valor J %.2f\n", vetor[l], vetor[j]);
 
-	#pragma omp parallel private(l) shared(i,j,sequencia,vetor)
-	{
-		#pragma omp for
-		for (int l = 0; l < sequencia; l++) {
-	  		if (debug == 1)
-	  			printf("Valor I %.2f valor J %.2f\n", vetor[l], vetor[j]);
-	  		
-	  		valorSoma = valorSoma + pow((vetor[i + l] - vetor[j +l]),2);
-		}
-	}
-	
-	raiz = sqrt(valorSoma);
+      valorSoma = valorSoma + pow((vetor[i + l] - vetor[j + l]), 2);
+    }
+  }
 
+  raiz = sqrt(valorSoma);
 
-  	if (debug)
-  		printf("%.1f\n", raiz);
-  	
-  	return raiz;
+  if (debug)
+    printf("%.1f\n", raiz);
+
+  return raiz;
 }
 
-void extraiVizinhoMaisProximo(
-	int tamanho,
-	double **vetorResultadoValoresDistancia,
-	float *vetorVizinhoMaisProximo,
-	int *vetorIndicesDistancia,
-	int debug
-){
-	int i, j;
-	#pragma omp parallel private(i,j) shared(vetorResultadoValoresDistancia, vetorVizinhoMaisProximo,vetorIndicesDistancia)
-	{
-		#pragma omp for
-		for (int i = 0; i  < tamanho; i++) {
-  			float valorMaisProximo = vetorResultadoValoresDistancia[i][0];
-   	    	int indice = 0;		
-        
-			for(int j = 1; j  < tamanho; j++) {
-				if(valorMaisProximo < vetorResultadoValoresDistancia[i][j]){
-			    	if (valorMaisProximo == 0){
-			    		if (vetorResultadoValoresDistancia[i][j] != 0) {
-			        		valorMaisProximo = vetorResultadoValoresDistancia[i][j];
-			       			indice = j + 1;
-			      		}
-			    	}
-			  	} else {
-			    	if (vetorResultadoValoresDistancia[i][j] != 0) {
-			       		valorMaisProximo = vetorResultadoValoresDistancia[i][j];
-			       		indice = j + 1;
-			    	}
-			  	}
-			}
-       
-        	vetorVizinhoMaisProximo[i] = valorMaisProximo;
-        	vetorIndicesDistancia[i] = indice;
-       
-       		if (debug == 1) {
-        		printf("Valor raiz vizinho mais proximo: %.1f\n", vetorVizinhoMaisProximo[i]);
-        		printf("Valor indice vizinho mais proximo:  %d\n", vetorIndicesDistancia[i]);	
-       		}
-    	}
-  	}
+int *extraiVizinhoMaisProximo(int tamanho,
+                              double **vetorResultadoValoresDistancia,
+                              float *vetorVizinhoMaisProximo, int debug) {
+  int i, j;
+
+  // Vetor que armazenas os índices do vizinho mais próximo
+  int *vetorIndicesDistancia = malloc(tamanho * sizeof(int *));
+
+#pragma omp parallel private(i, j)                                             \
+    shared(vetorResultadoValoresDistancia, vetorVizinhoMaisProximo,            \
+           vetorIndicesDistancia)
+  {
+#pragma omp for
+    for (int i = 0; i < tamanho; i++) {
+      float valorMaisProximo = vetorResultadoValoresDistancia[i][0];
+      int indice = 0;
+
+      for (int j = 1; j < tamanho; j++) {
+        if (valorMaisProximo < vetorResultadoValoresDistancia[i][j]) {
+          if (valorMaisProximo == 0) {
+            if (vetorResultadoValoresDistancia[i][j] != 0) {
+              valorMaisProximo = vetorResultadoValoresDistancia[i][j];
+              indice = j + 1;
+            }
+          }
+        } else {
+          if (vetorResultadoValoresDistancia[i][j] != 0) {
+            valorMaisProximo = vetorResultadoValoresDistancia[i][j];
+            indice = j + 1;
+          }
+        }
+      }
+
+      vetorVizinhoMaisProximo[i] = valorMaisProximo;
+      vetorIndicesDistancia[i] = indice;
+
+      if (debug == 1) {
+        printf("Valor raiz vizinho mais proximo: %.1f\n",
+               vetorVizinhoMaisProximo[i]);
+        printf("Valor indice vizinho mais proximo:  %d\n",
+               vetorIndicesDistancia[i]);
+      }
+    }
+  }
+
+  return vetorIndicesDistancia;
 }
